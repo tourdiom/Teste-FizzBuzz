4c75c3535a3ba73f373d1b910a7f9933dae66fe4
tourdiom <mx_santos@hotmail.com>
2021-06-10 17:01:01 -0300

    Initial commit

---
+++ TesteFizzBuzz.py
@@ -0,0 +1,20 @@
+
+
+def FizzBuzz(n1):  # Função sobre numeros divisiveis por 3 e 5
+    if n1 % 5 == 0 and n1 % 3 == 0:
+        return f'FizzBuzz, {n1} é divisivel por 3 e por 5'
+
+    elif n1 % 3 == 0:
+        return f'Fizz, {n1} é divisivel por 3'
+    elif n1 % 5 == 0:
+        return f'Buzz, {n1} é divisivel por 5'
+
+    else:
+        return f'{n1} Não é divisivel nem por 3 e nem por 5'
+
+
+from random import randint # função para retornar numeros aleatorios em um determinado range, descrito a baixo
+
+for i in range(100):
+    aleatorio = randint(0,100)
+    print(FizzBuzz(aleatorio))
